333d06bbb1f29d7d993bbcd4f1903dfad0e998f1
Md Rakib Hossain <rakibuits1059@gmail.com>
2022-05-23 11:53:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    Navbar
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default App;
